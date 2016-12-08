fe6ca877f91051bade46a78903b182765e479dc2
Lam Nguyen <ltnguyen14@Lams-MacBook-Pro.local>
2016-12-08 13:06:18 -0500

    ver 0.1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <sstream>
 #include <iostream>
 
 
@@ -14,29 +13,85 @@ int interval = 1000/60;
 int score_left = 0;
 int score_right = 0;
 
-// rackets in general
 int paddle_width = 10;
 int paddle_height = 80;
 int paddle_speed = 10;
 
-// left racket position
 float paddle_left_x = 10.0f;
 float paddle_left_y = 50.0f;
 
-// right racket position
 float paddle_right_x = screenWidth - paddle_width - 10;
 float paddle_right_y = 50;
 
 float ball_pos_x = screenWidth / 2;
 float ball_pos_y = screenHeight / 2;
-float ball_dir_x = 0.0f;
-float ball_dir_y = 1.0f;
+float ball_dir_x = 1.0f;
+float ball_dir_y = 0.0f;
 int ball_size = 10;
 int ball_speed = 2;
+
+void vec2_norm(float& x, float &y) {
+        float length = sqrt((x * x) + (y * y));
+        if (length != 0.0f) {
+            length = 1.0f / length;
+            x *= length;
+            y *= length;
+        }
+    }
 
 void update_ball(){
   ball_pos_x += ball_dir_x * ball_speed;
   ball_pos_y += ball_dir_y * ball_speed;
+
+  // check left paddle
+    if (ball_pos_x <= paddle_left_x + paddle_width &&
+        ball_pos_x >= paddle_left_x &&
+        ball_pos_y <= paddle_left_y + paddle_height &&
+        ball_pos_y >= paddle_left_y) {
+        float t = ((ball_pos_y - paddle_left_y) / paddle_height) - 0.5f;
+        ball_dir_x = fabs(ball_dir_x);
+        ball_dir_y = t;
+    }
+
+    // check right paddle
+    if (ball_pos_x >= paddle_right_x &&
+        ball_pos_x <= paddle_right_x + paddle_width &&
+        ball_pos_y <= paddle_right_y + paddle_height &&
+        ball_pos_y >= paddle_right_y) {
+        float t = ((ball_pos_y - paddle_right_y) / paddle_height) - 0.5f;
+        ball_dir_x = -fabs(ball_dir_x);
+        ball_dir_y = t;
+    }
+
+    // check left
+    if (ball_pos_x < 0) {
+        ++score_right;
+        ball_pos_x = screenWidth / 2;
+        ball_pos_y = screenHeight / 2;
+        ball_dir_x = fabs(ball_dir_x);
+        ball_dir_y = 0;
+    }
+
+    // check right
+    if (ball_pos_x > screenWidth) {
+        ++score_left;
+        ball_pos_x = screenWidth / 2;
+        ball_pos_y = screenHeight / 2;
+        ball_dir_x = -fabs(ball_dir_x);
+        ball_dir_y = 0;
+    }
+
+    // check top
+    if (ball_pos_y > screenHeight) {
+        ball_dir_y = -fabs(ball_dir_y);
+    }
+
+    // check bottom
+    if (ball_pos_y < 0) {
+        ball_dir_y = fabs(ball_dir_y);
+    }
+
+    vec2_norm(ball_dir_x, ball_dir_y);
 }
 
 void draw_paddle(float x, float y, float width, float height) {
@@ -85,10 +140,12 @@ void display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
+    // Draw two paddles
     draw_paddle(paddle_left_x, paddle_left_y, paddle_width, paddle_height);
     draw_paddle(paddle_right_x, paddle_right_y, paddle_width, paddle_height);
 
-    draw_paddle(ball_pos_x - ball_size / 2, ball_pos_y - ball_size / 2, ball_size, ball_size);
+    // Draw the ball
+    draw_paddle(ball_pos_x, ball_pos_y, ball_size, ball_size);
 
     print(screenWidth / 2 - 120, screenHeight - 30, "The best Pong game ever");
 
